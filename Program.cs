ceda3e629792abbc6db47939891abe0a26b2fa07
KaanBurulday <105288084+KaanBurulday@users.noreply.github.com>
2024-04-19 00:45:16 +0300

    itr6

--- Program.cs
+++ Program.cs
@@ -34,14 +34,14 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "University Tuition Payment API");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "University Tuition Payment API");
+});
+
 
 app.UseHttpsRedirection();
 
